f4fb67237d1af7f396b3a6dddb6e15348804d12d
arin <stefan@cherehapa.ru>
2019-08-20 00:18:58 +0300

    Add loader & rename pages

--- app/src/routes.tsx
+++ app/src/routes.tsx
@@ -1,13 +1,14 @@
 import React, { Suspense, lazy } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { FallbackLoader } from '@common/styles';
 
-const ChatPage = lazy(() => import('@pages/Chat'));
+const ChatPage = lazy(() => import('@pages/ChatPage'));
 const MainPage = lazy(() => import('@pages/MainPage'));
 
-//todo: Loader for fallback;
+//todo: Add good loader for fallback (or skeleton);
 
 const routes = (
-   <Suspense fallback={<div>Загрузка...</div>}>
+   <Suspense fallback={<FallbackLoader><img src="https://i.pinimg.com/originals/a7/12/3a/a7123a124ba35c74c421e1678e2bb677.gif" /></FallbackLoader>}>
       <Switch>
         <Route exact path="/" component={MainPage} />
         <Route path="/chat/:type" component={ChatPage} />
